33f7c860d590382f55fe4d38f0e4ba7af950a88a
Camilo Mora <camiloalf23@gmail.com>
2025-06-01 14:33:26 -0500

    Archivos iniciales

---
+++ src/main/java/com/encriptador/EncrypterMylo/EncrypterMyloApplication.java
@@ -0,0 +1,13 @@
+package com.encriptador.EncrypterMylo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncrypterMyloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncrypterMyloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encriptador/EncrypterMylo/EncrypterMyloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encriptador.EncrypterMylo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncrypterMyloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
